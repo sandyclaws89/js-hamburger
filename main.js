181cde5502b1fbae846fae8903ae4ba21009b72d
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-22 19:46:50 +0100

    compare solo il contenuto come se non ci fosse lo stile applicato e mi fa cliccare solo una volta

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// HO ASSOCIATO LA VARIABILE HamburgerMenu ALLA CLASSE hamburger-menu 
 let HamburgerMenu = document.querySelector('.hamburger-menu');
+
+// HO ASSOCIATO A button LA CLASSE FA-BARS(L'SVG HAMBURGER)
 let button = document.querySelector('.fa-bars');
 
+// ASCOLTA SE AVVIENE UN CLICK SU BUTTON, IN QUEL CASO, AD OGNI CLICK TOGLI E RIMETTI LA CLASSE hamburger-menu SULLA MIA VARIABILE HamburgerMenu
 button.addEventListener('click', function() {
     HamburgerMenu.classList.toggle('hamburger-menu')
+    // PERCHE' QUI ORA NON C'E' BISOGNO DEL PUNTO PRIMA DELLA CLASSE E PRIMA SI?
 })
 
 
